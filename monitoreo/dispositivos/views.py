e172fdae31976f3b9a73bf1749b4cef6733a4972
Pedro Gómez <pedrogomezzambra182@gmail.com>
2025-08-22 11:51:29 -0400

    solo agregue lo inicial pero aun falta xd

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, "dispositivos/inicio.html")
+
+def panel_dispositivos(request):
+    dispositivos = [
+        {"nombre": "sensor temperatura", "consumo": 50},
+        {"nombre": "Medidor Solar", "consumo": 120},
+        {"nombre": "Sensor movimiento", "consumo": 30},
+        {"nombre": "Calefactor", "consumo": 200},
+    ]
+
+    consumo_maximo = 100
+
+    return render(request, "dispositivos/inicio.html", {
+        "dispositivos": dispositivos,
+        "consumo_maximo": consumo_maximo
+    })
